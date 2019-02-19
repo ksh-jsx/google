44beb6d32916bcb5c1a3ec3f745d8e984d238faa
shkim787 <shkim061198@gmail.com>
2019-02-20 01:17:36 +0900

    뒤로가기 추가

--- include/js/main.js
+++ include/js/main.js
@@ -1,8 +1,19 @@
 function mkPlan()
 {
-
+    document.getElementById("cancel").style.display="block";
+    document.getElementById("add").style.display="none";
+    document.getElementById("backbtn").style.display="block";
+    document.getElementById("plusbtn").style.display="none";
     document.getElementById("addInfo").style.display="block";
     document.getElementById("timeLine").style.display="none";
-
 }
 
+function back()
+{
+    document.getElementById("cancel").style.display="none";
+    document.getElementById("add").style.display="block";
+    document.getElementById("backbtn").style.display="none";
+    document.getElementById("plusbtn").style.display="block";
+    document.getElementById("addInfo").style.display="none";
+    document.getElementById("timeLine").style.display="block";
+}
